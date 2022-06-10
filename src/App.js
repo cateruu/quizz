c0e65273ea798802c7a916eb3a1434a895d68583
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-10 10:28:54 +0200

    🤬third try to make quiz work !@#!@#!@#

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import classes from './app.module.css';
 import Landing from './components/Landing';
-import Quiz from './components/Quiz';
 
 const App = () => {
   const startGame = () => {
@@ -10,29 +9,12 @@ const App = () => {
   };
 
   const [gameStarted, setGameStarted] = useState(false);
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    const getQuestions = async () => {
-      const response = await fetch(
-        'https://opentdb.com/api.php?amount=5&type=multiple'
-      );
-      const result = await response.json();
-
-      setQuestions(result.results);
-    };
-
-    getQuestions();
-  }, [gameStarted]);
-
-  console.log(questions);
 
   return (
     <div className={classes.app}>
       <div className={classes.topBlob}></div>
       <div className={classes.bottomBlob}></div>
       {!gameStarted && <Landing handleClick={startGame} />}
-      {gameStarted && <Quiz questions={questions} />}
     </div>
   );
 };
